6a976ca21b2ce85336f1f7fe1e5a4ca0f9d67ebc
Maiser <maiseralves@gmail.com>
2016-11-29 10:14:08 -0200

    Corrigido leitura de string com espaço

--- class/InputReader.class.php
+++ class/InputReader.class.php
@@ -13,19 +13,24 @@ class InputReader{
 		$this->file_handler = fopen('php://stdin', 'r');
 	}
 
-	/**
-		Exibe na tela a mensagem e aguarda o valor de entrada do tipo $type 
-
-		@param $message Mensagem a ser exibida na tela
-		@param $type O tipo do valor que se deseja ler segundo o formato 
-					 especificado em http://php.net/manual/pt_BR/function.sprintf.php
-		@return Retorna o valor lido pelo terminal no formato especificado por $type;      
-	**/
+	/*
+	 *	Exibe na tela a mensagem e aguarda o valor de entrada do tipo $type 
+	 *
+	 *	@param $message Mensagem a ser exibida na tela
+	 *	@param $type O tipo do valor que se deseja ler segundo o formato 
+	 *				 especificado em http://php.net/manual/pt_BR/function.sprintf.php
+	 *  @return Retorna o valor lido pelo terminal no formato especificado por $type;      
+	 */
 	public function read($message, $type='%s'){
 
 		print($message);
-		fscanf($this->file_handler, $type, $rvar);
 
+		if($type != '%s'){
+			fscanf($this->file_handler, $type, $rvar);
+		}else{
+			$rvar = fgets($this->file_handler, 255);
+			$rvar = str_replace("\n", "", $rvar);
+		}
 		return $rvar;
 	}
 
